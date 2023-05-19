d71957c9d3885feccaf89bd0ac09d85031777da2
SB-202157271 <86137106+sbucarion@users.noreply.github.com>
2023-05-19 14:19:14 -0400

    Delete backend directory

--- backend/backend/__init__.py
+++
@@ -1,4 +0,0 @@
-#Must use when using pymysql so django can understand it as mysqldb 
-
-import pymysql
-pymysql.install_as_MySQLdb()
--- backend/user_auth/auth_api/serializers.py
+++
@@ -1,2 +0,0 @@
-#Used for turning backend data into json data tosend to frontend
-#To make javascript understyand pythno data
